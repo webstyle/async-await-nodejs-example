5cdceeeb17bbc51d246f683ee7d81510faf1aa9e
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-12-08 23:17:12 +0500

    added promise error wrapper method

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const TicketsDao = require('./TicketsDao');
 class App {
   async main() {
     const dao = new TicketsDao();
-    try {
-      let list = await dao.getAll(100);
-      let ticket = await dao.findOne(list[10].ticket_no);
-      console.log(ticket);
-    } catch (error) {
-      console.log('Error, on TicketsDao.getAll(). Message '+ error.message);
-    }
+    let [errorList, list] = await this.to(dao.getAll(100));
+    if (errorList !== null) return console.error('Error list');
+
+    let [errorTicket, ticket] = await this.to(dao.findOne(list[10].ticket_no));
+    if (errorTicket) return console.error('Error ticket');
+
+    console.log(ticket);
+  }
+
+  async to(promise) {
+    return promise
+      .then((response) => [null, response])
+      .catch((error) => [error]);
   }
 }
 
