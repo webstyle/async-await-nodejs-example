7fb3a62d2336b93a688964b8617f2215aee9dcc4
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-12-08 21:48:14 +0500

    added promise examples

--- promise/index.js
+++ promise/index.js
@@ -1,6 +1,14 @@
+const TicketsDao = require('../TicketsDao');
+
 class App {
   main() {
+    const dao = new TicketsDao();
 
+    dao.getAll(100)
+      .then((response) => dao.findOne(response[0].ticket_no))
+      .catch((error) => {
+        console.log('Error, on TicketsDao.getAll(). Message '+ error.message);
+      })
   }
 }
 
